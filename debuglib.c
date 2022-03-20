8ddcb4cf3051bb2599550cb8531be4f7c1f4f8ce
Katia-ML <moualikatia@gmail.com>
2022-03-20 21:33:07 +0000

    Ajout code debuglib.c

---
+++ debuglib.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/reg.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "debuglib.h"
+
+
+/* Print a message to stdout, prefixed by the process ID
+*/
+void procmsg(const char* format, ...)
+{
+    va_list ap;
+    fprintf(stdout, "[%d] ", getpid());
+    va_start(ap, format);
+    vfprintf(stdout, format, ap);
+    va_end(ap);
+}
+
+
+/* Run a target process in tracing mode by exec()-ing the given program name.
+*/
+void run_target(const char* programname)
+{
+    procmsg("target started. will run '%s'\n", programname);
+
+    /* Allow tracing of this process */
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+        perror("ptrace");
+        return;
+    }
+
+    /* Replace this process's image with the given program */
+    execl(programname, programname, NULL);
+}
+
+
+long get_child_eip(pid_t pid)
+{
+    struct user_regs_struct regs;
+    ptrace(PTRACE_GETREGS, pid, 0, &regs);
+    return regs.rip;
+}
+
+
